5cf7029272785a3503cab4e30ffd115813f4196c
Tiv Barlow <78834066+TivBit@users.noreply.github.com>
2023-01-26 20:38:46 -0500

    GPA program python file added.
    For discussion board peer critique.

---
+++ barlowNativida_M02 Lab_Case Study_if_else and while.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on %(Date: 26 Jan 2023)s
+
+@author: %(Nativida Barlow)s
+Python Version 3.11.1
+SDEV 220 Spring 2023
+Assignment: M02 Lab - Case Study: if...else and while
+Program Description:  This is program collects student name and grade
+information using if and else to determine if they made the Dean's list or the Honor Roll.
+"""
+
+student_first_name = ""
+student_last_name = ""
+gpa = float
+
+
+student_last_name = input("What is the student's last name? ")
+
+if student_last_name == "ZZZ":
+    print("Error, the last name you entered cannot be 'ZZZ'.  Please re-enter last name. ")
+
+
+student_first_name = input("What is the student's first name? ")
+print("Recheiving records for " + student_first_name + " " + student_last_name + ".  Please stand by...")
+
+gpa = float(input("What is " + student_first_name + "'s GPA? "))
+
+if gpa >= 3.5:
+    print(student_first_name + " " + student_last_name + " is on the Dean's List.")
+    
+elif gpa >= 3.25:
+    print(student_first_name + " " + student_last_name + " made the Honor Roll.")
+    
+else:
+    print(student_first_name + " " + student_last_name + " did not make the Dean's List or the Honor Roll.")
+        
+    
+
+
+
+
